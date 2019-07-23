40fd0981d8080adddc5d7a759f8720dd68291847
Javier Gómez Luzón <javo14795@gmail.com>
2019-07-23 23:17:44 +0200

    Add user options

--- src/app/pages/user/user.module.ts
+++ src/app/pages/user/user.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -18,6 +18,7 @@ import { UserOptionsPage } from './user-options/user-options.page';
     CommonModule,
     FormsModule,
     IonicModule,
+    ReactiveFormsModule,
     RouterModule.forChild([
       {
         path: 'main',
