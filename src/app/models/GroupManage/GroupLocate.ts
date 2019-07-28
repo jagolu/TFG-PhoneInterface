df932ccd02ae5a6adbbfbf2f852e362523c1c887
Javier Gómez Luzón <javo14795@gmail.com>
2019-07-28 11:42:03 +0200

    Add groupLocate

---
+++ src/app/models/GroupManage/GroupLocate.ts
@@ -0,0 +1,6 @@
+import { GroupTab } from './GroupTab';
+
+export interface GroupLocate{
+    name:string;
+    tab:GroupTab;
+}
---
+++ src/app/models/GroupManage/GroupTab.ts
@@ -0,0 +1,10 @@
+export enum GroupTab{
+    NEWS = "Noticias",
+    NEW_BETS = "Nuevas apuestas",
+    ACTIVE_BETS = "Apuestas activas",
+    MANAGE_BETS = "Gestionar apuestas",
+    HISTORY = "Historial",
+    MEMBERS = "Miembros",
+    OPTIONS = "Opciones",
+    INFO = "Info"
+}
