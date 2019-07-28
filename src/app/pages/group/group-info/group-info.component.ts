2aa1fcf4018a43968b39c9c4436cb5e2e706c304
Javier Gómez Luzón <javo14795@gmail.com>
2019-07-28 17:22:31 +0200

    Add group info tab

--- src/app/pages/group/group-info/group-info.component.ts
+++ src/app/pages/group/group-info/group-info.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { GroupInfoService } from 'src/app/providers/userServices/group-info.service';
+import { GroupService } from 'src/app/providers/restServices/group.service';
+import { IconModel, Icons } from 'src/app/models/models';
 
 @Component({
   selector: 'app-group-info',
@@ -7,8 +10,139 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupInfoComponent implements OnInit {
 
-  constructor() { }
+  //
+  // ──────────────────────────────────────────────────────────────────────
+  //   :::::: C L A S S   V A R S : :  :   :    :     :        :          :
+  // ──────────────────────────────────────────────────────────────────────
+  //
 
-  ngOnInit() {}
+  /**
+   * The name of the group
+   * 
+   * @access public
+   * @var {string} groupName
+   */
+  public groupName:string;
 
+  /**
+   * The join date of the actual user
+   * to the group
+   * 
+   * @access public
+   * @var {string} dateJoin
+   */
+  public dateJoin:string;
+
+  /**
+   * The role date of the actual user
+   * 
+   * @access public
+   * @var {string} dateRole
+   */
+  public dateRole:string;
+
+  /**
+   * Max capacity of the group
+   * 
+   * @access public
+   * @var {number} maxCapacity
+   */
+  public maxCapacity:number;
+
+  /**
+   * The actual capacity of the group
+   * 
+   * @access public
+   * @var {number} actualCapacity
+   */
+  public actualCapacity:number;
+
+  /**
+   * The date when the group was created
+   * 
+   * @access public
+   * @var {string} createDate
+   */
+  public createDate:string;
+
+  /**
+   * The role of the user in the group
+   * 
+   * @access public
+   * @var {string} role 
+   */
+  public role:string;
+
+  /**
+   * A icon of a crown
+   * 
+   * @access public
+   * @var {IconModel} icon_crown
+   */
+  public icon_crown:IconModel = Icons.CROWN;
+
+  /**
+   * A icon of a wizard hat
+   * 
+   * @access public
+   * @var {IconModel} icon_wizard
+   */
+  public icon_wizard:IconModel = Icons.WIZARD;
+
+  /**
+   * A icon of a normal user
+   * 
+   * @access public
+   * @var {IconModel} icon_user
+   */
+  public icon_user:IconModel = Icons.USER;
+
+
+  //
+  // ──────────────────────────────────────────────────────────────────────────
+  //   :::::: C O N S T R U C T O R S : :  :   :    :     :        :          :
+  // ──────────────────────────────────────────────────────────────────────────
+  //
+
+  /**
+   * @constructor
+   * @param {GroupInfoService} __groupInfoS To get the info of the group
+   * @param {GroupService} __groupS To do group http requests
+   */
+  constructor(private __groupInfoS:GroupInfoService, private __groupS:GroupService) { }
+
+  /**
+   * Gets the info of the group
+   * 
+   * @OnInit
+   */
+  ngOnInit() {
+    this.__groupInfoS.info.subscribe(page=>{
+      try{
+        this.role = page.members ? page.members[page.members.length-1].role : "";
+        this.groupName = page.name;
+        this.dateJoin = page.dateJoin;
+        this.dateRole = page.dateRole;
+        this.maxCapacity = page.maxCapacity;
+        this.actualCapacity = page.actualCapacity;
+        this.createDate = page.createDate;
+      }catch(Error){}
+    });
+  }
+
+
+  //
+  // ──────────────────────────────────────────────────────────────────────────────────
+  //   :::::: P U B L I C   F U N C T I O N S : :  :   :    :     :        :          :
+  // ──────────────────────────────────────────────────────────────────────────────────
+  //
+
+  /**
+   * Leaves the group
+   * 
+   * @access public
+   */
+  public leaveGroup(){
+    this.__groupS.leaveGroup(this.groupName);
+  }
 }
