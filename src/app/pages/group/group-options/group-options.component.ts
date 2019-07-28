02b7a72cb65ccc440c0a9eca7aae8f94e0bc961d
Javier Gómez Luzón <javo14795@gmail.com>
2019-07-28 20:19:33 +0200

    Add basic group options

--- src/app/pages/group/group-options/group-options.component.ts
+++ src/app/pages/group/group-options/group-options.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertService } from 'src/app/providers/visualServices/alert.service';
+import { GroupInfoService } from 'src/app/providers/userServices/group-info.service';
 
 @Component({
   selector: 'app-group-options',
@@ -7,8 +9,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupOptionsComponent implements OnInit {
 
-  constructor() { }
+  //
+  // ──────────────────────────────────────────────────────────────────────
+  //   :::::: C L A S S   V A R S : :  :   :    :     :        :          :
+  // ──────────────────────────────────────────────────────────────────────
+  //
 
-  ngOnInit() {}
+  /**
+   * The name of  the group
+   * 
+   * @access private
+   * @var {string} _groupName
+   */
+  private _groupName:string;
 
+
+  //
+  // ──────────────────────────────────────────────────────────────────────────
+  //   :::::: C O N S T R U C T O R S : :  :   :    :     :        :          :
+  // ──────────────────────────────────────────────────────────────────────────
+  //
+
+  /**
+   * @constructor
+   * @param {AlertService} __alertS To launch the remove group alert
+   * @param {GroupInfoService} __groupInfoS To get the name of the group
+   */
+  constructor(private __alertS:AlertService, private __groupInfoS:GroupInfoService){}
+
+  /**
+   * Gets the name of the group
+   * 
+   * @OnInit
+   */
+  ngOnInit(){
+    this.__groupInfoS.info.subscribe(page=>{
+      try{ this._groupName = page.name; }
+      catch(Error){ this._groupName = "" }
+    });
+  }
+
+
+  //
+  // ──────────────────────────────────────────────────────────────────────────────────
+  //   :::::: P U B L I C   F U N C T I O N S : :  :   :    :     :        :          :
+  // ──────────────────────────────────────────────────────────────────────────────────
+  //
+
+  /**
+   * Launchs the remove group alert
+   * 
+   * @access public
+   */
+  public openDeleteGroupAlert(){
+    this.__alertS.deleteGroup(this._groupName);
+  }
 }
