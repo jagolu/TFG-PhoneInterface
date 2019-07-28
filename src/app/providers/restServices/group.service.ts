0f3595d78da93d65cba5df0d6b06cb58c13c7b4e
Javier Gómez Luzón <javo14795@gmail.com>
2019-07-28 20:02:54 +0200

    Update group.service.ts

--- src/app/providers/restServices/group.service.ts
+++ src/app/providers/restServices/group.service.ts
@@ -172,6 +172,8 @@ export class GroupService extends Rest{
    * Give to another user in a group the role of group admin
    * 
    * @access public
+   * @param {MakeUnmake_admin_block} order The order to make or unmake 
+   * the user 
    */
   public makeAdmin(order:MakeUnmake_admin_block){
     this.postRequest(order, this.__groupPath+"MakeAdmin").subscribe(
@@ -183,6 +185,7 @@ export class GroupService extends Rest{
    * Kick the user from the group
    * 
    * @access public
+   * @param {KickUser} order The order to kick the user
    */
   public kickUser(order:KickUser){
     this.postRequest(order, this.__groupPath+"RemoveUser").subscribe(
@@ -194,6 +197,8 @@ export class GroupService extends Rest{
    * Kick the user from the group
    * 
    * @access public
+   * @param {MakeUnmake_admin_block} order The order to block or
+   * unblock the user
    */
   public blockUser(order:MakeUnmake_admin_block){
     this.postRequest(order, this.__groupPath+"BlockUser").subscribe(
@@ -205,6 +210,8 @@ export class GroupService extends Rest{
    * Set a password to the group
    * 
    * @access public
+   * @param {ManagePassword} order The order to manage the 
+   * password of the group 
    */
   public managePassword(order:ManagePassword){
     this.postRequest(order, this.__groupPath+"ManagePassword").subscribe(
@@ -213,9 +220,10 @@ export class GroupService extends Rest{
   }
 
   /**
-   * Renmoves a group
+   * Removes a group
    * 
    * @access public
+   * @param {RemoveGroup} order The order to remove group
    */
   public removeGroup(order:RemoveGroup){
     this.postRequest(order, this.__groupPath+"RemoveGroup").subscribe(
