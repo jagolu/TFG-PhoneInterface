54a05864a488d98dec3911d93b37aaa85ebb3ef7
Javier Gómez Luzón <javo14795@gmail.com>
2019-07-30 21:10:34 +0200

    Add groupnewbet

--- src/app/pages/group/group-new-bet/group-new-bet.component.ts
+++ src/app/pages/group/group-new-bet/group-new-bet.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { GroupBet } from 'src/app/models/models';
+import { GroupInfoService } from 'src/app/providers/userServices/group-info.service';
+import { AlertService } from 'src/app/providers/visualServices/alert.service';
 
 @Component({
   selector: 'app-group-new-bet',
@@ -7,8 +10,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupNewBetComponent implements OnInit {
 
-  constructor() { }
+  //
+  // ──────────────────────────────────────────────────────────────────────
+  //   :::::: C L A S S   V A R S : :  :   :    :     :        :          :
+  // ──────────────────────────────────────────────────────────────────────
+  //
 
-  ngOnInit() {}
+  /**
+   * The new bets in the group
+   * 
+   * @access public
+   * @var {GroupBet[]} bets
+   */
+  public bets:GroupBet[] = [];
 
+  /**
+   * The coins of the actual user
+   * 
+   * @access public
+   * @var {number} userCoins
+   */
+  public userCoins:number = 0;
+
+
+  //
+  // ──────────────────────────────────────────────────────────────────────────
+  //   :::::: C O N S T R U C T O R S : :  :   :    :     :        :          :
+  // ──────────────────────────────────────────────────────────────────────────
+  //
+
+  /**
+   * @constructor
+   * @param {GroupInfoService} __groupInfoS To get the info of the group
+   * @param {AlertService} __alertS To launch the alerts
+   */
+  constructor(private __groupInfoS:GroupInfoService, private __alertS:AlertService) { }
+
+  /**
+   * Gets the bets from the service
+   * 
+   * @OnInit
+   */
+  ngOnInit() {
+    this.__groupInfoS.info.subscribe(page=>{
+      try{
+        this.userCoins =  page.members ? page.members[page.members.length-1].coins : 0;
+        this.bets = page.bets;
+      }catch(Error){}
+    });
+  }
+
+
+  //
+  // ──────────────────────────────────────────────────────────────────────────────────
+  //   :::::: P U B L I C   F U N C T I O N S : :  :   :    :     :        :          :
+  // ──────────────────────────────────────────────────────────────────────────────────
+  //
+
+  /**
+   * Launchs the alert to see a user football bet
+   * 
+   * @access public
+   * @param {GroupBet} bet The bet which the user want to see 
+   */  
+  public seeBet(bet:GroupBet){
+  }
 }
