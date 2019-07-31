14baf9bfc9e3c811722319337b7b18d32fe5927a
Javier Gómez Luzón <javo14795@gmail.com>
2019-07-31 20:24:53 +0200

    Add view & funcionality for group history

--- src/app/pages/group/group-history/group-history.component.ts
+++ src/app/pages/group/group-history/group-history.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { EndedFootballBet, GroupBet, HistoryUserFootballBet } from 'src/app/models/models';
+import { GroupInfoService } from 'src/app/providers/userServices/group-info.service';
+import { AlertService } from 'src/app/providers/visualServices/alert.service';
 
 @Component({
   selector: 'app-group-history',
@@ -7,8 +10,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupHistoryComponent implements OnInit {
 
-  constructor() { }
+  //
+  // ──────────────────────────────────────────────────────────────────────
+  //   :::::: C L A S S   V A R S : :  :   :    :     :        :          :
+  // ──────────────────────────────────────────────────────────────────────
+  //
 
-  ngOnInit() {}
+  /**
+   * The history of bets of the user
+   * 
+   * @access public
+   * @var {EndedFootballBet[]} betsHistory
+   */
+  public betsHistory:EndedFootballBet[];
 
+
+  //
+  // ──────────────────────────────────────────────────────────────────────────
+  //   :::::: C O N S T R U C T O R S : :  :   :    :     :        :          :
+  // ──────────────────────────────────────────────────────────────────────────
+  //
+
+  /**
+   * @constructor
+   * @param {GroupInfoService} __groupInfoS To get the history-bets
+   */
+  constructor(private __groupInfoS:GroupInfoService, private __alertS:AlertService) { }
+
+  /**
+   * Gets the history-bets
+   * 
+   * @OnInit
+   */
+  ngOnInit() {
+    this.__groupInfoS.info.subscribe(page=>{
+      try{ this.betsHistory = page.betsHistory;}
+      catch(Error){this.betsHistory = []}
+    });
+  }
+
+
+  //
+  // ──────────────────────────────────────────────────────────────────────────────────
+  //   :::::: P U B L I C   F U N C T I O N S : :  :   :    :     :        :          :
+  // ──────────────────────────────────────────────────────────────────────────────────
+  //
+  /**
+   * Launchs the alert to see a user football bet
+   * 
+   * @access public
+   * @param {GroupBet} bet The bet which the user want to see 
+   */  
+  public seeBet(bet:GroupBet){
+    this.__alertS.seeFootballBet(bet, "", true);
+  }
+  
+  /**
+   * Launchs the alert to see the user bets from someone
+   * 
+   * @access public
+   * @param {GroupBet} fb The football bet 
+   * @param {HistoryUserFootballBet} bets The user bets  
+   * @param {string} name The name of the user
+   */
+  public seeUserBets(fb:GroupBet, bets:HistoryUserFootballBet[], name:string){
+    let alertName = name=="" ? "Tus apuestas" : "Apuestas de "+name;
+    this.__alertS.seeUserBet(bets, fb, true, alertName);
+  }
 }
