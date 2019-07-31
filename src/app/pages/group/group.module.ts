1b5ff7d9692d732034d9ca61d87b6d4eb64ad9ca
Javier Gómez Luzón <javo14795@gmail.com>
2019-07-31 23:25:01 +0200

    Update manage bets

--- src/app/pages/group/group.module.ts
+++ src/app/pages/group/group.module.ts
@@ -7,7 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { GroupNewsComponent } from './group-news/group-news.component';
 import { GroupNewBetComponent } from './group-new-bet/group-new-bet.component';
 import { GroupActiveBetsComponent } from './group-active-bets/group-active-bets.component';
-import { GroupMangeBetsComponent } from './group-mange-bets/group-mange-bets.component';
+import { GroupManageBetsComponent } from './group-manage-bets/group-manage-bets.component';
 import { GroupHistoryComponent } from './group-history/group-history.component';
 import { GroupOptionsComponent } from './group-options/group-options.component';
 import { GroupInfoComponent } from './group-info/group-info.component';
@@ -41,7 +41,7 @@ import { WeeklyPayFormComponent } from './group-options/weekly-pay-form/weekly-p
       },
       {
         path: 'manageBets',
-        component: GroupMangeBetsComponent
+        component: GroupManageBetsComponent
       },
       {
         path: 'history',
@@ -65,7 +65,7 @@ import { WeeklyPayFormComponent } from './group-options/weekly-pay-form/weekly-p
     GroupNewsComponent,
     GroupNewBetComponent,
     GroupActiveBetsComponent,
-    GroupMangeBetsComponent,
+    GroupManageBetsComponent,
     GroupHistoryComponent,
     GroupOptionsComponent,
     GroupInfoComponent,
